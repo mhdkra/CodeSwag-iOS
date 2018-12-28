64359f0da4e55884dc72e3a6a6e99473ec4c7d96
Tiara Mahardika <tiaramahardika@student.ub.ac.id>
2018-12-29 02:19:50 +0700

    create UITableView

---
+++ Code-Swag-iOS/Views/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  PeoductCell.swift
+//  Code-Swag-iOS
+//
+//  Created by Tiara Mahardika on 29/12/18.
+//  Copyright © 2018 Tiara Mahardika. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productTitle: UILabel!
+    
+}
